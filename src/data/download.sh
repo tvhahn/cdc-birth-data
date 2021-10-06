c301304b36f7f66ed77a3ee3c9fd6e017671819a
tvhahn <tvhahn@gmail.com>
2021-10-06 11:50:33 -0700

    fixed download.sh

--- src/data/download.sh
+++ src/data/download.sh
@@ -10,6 +10,6 @@ cd $PROJECT_DIR/data/raw
 # select years
 years=($(seq 1968 2019))
 
-for i in $(seq 0 14); do
+for i in $(seq 0 52); do
    wget https://data.nber.org/natality/"${years[$i]}"/natl"${years[$i]}".csv.zip 
 done
