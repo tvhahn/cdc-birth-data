33aa297fae9eccc73d057adea60fc46dd8aee0e2
tvhahn <tvhahn@gmail.com>
2021-10-23 23:26:19 -0400

    start make_hpc_data.sh for slurm

---
+++ src/data/make_hpc_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --time=01:00:00 # 1 hr
+#SBATCH --mem=4G
+#SBATCH --account=rrg-mechefsk
+#SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
+#SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
