b7314efbc09aeb028dee26999e03ed232572d1b2
tvhahn <tvhahn@gmail.com>
2021-10-24 20:48:02 -0400

    workign on data slurm script

--- src/data/make_hpc_data.sh
+++ src/data/make_hpc_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #SBATCH --time=01:00:00 # 1 hr
+#SBATCH --ntasks=1
+#SBATCH --cpus
 #SBATCH --mem=4G
 #SBATCH --account=rrg-mechefsk
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
