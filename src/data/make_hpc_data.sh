ecaf065a122d7e4ddb198b92cf9532d7e524873e
tvhahn <tvhahn@gmail.com>
2021-10-24 17:57:43 -0700

    update

--- src/data/make_hpc_data.sh
+++ src/data/make_hpc_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=01:00:00 # 1 hr
 #SBATCH --ntasks=1
-#SBATCH --cpus
-#SBATCH --mem=4G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=12G
 #SBATCH --account=rrg-mechefsk
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
