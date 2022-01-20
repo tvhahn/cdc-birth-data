f63a46e0d30abf1317b16df5bb2ee52e029a0247
tvhahn <tvhahn@gmail.com>
2022-01-19 21:11:48 -0800

    update

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -111,15 +111,25 @@ def percentage_birts_by_month(df, years_greater_than=1980):
     and percent above average (percent_above_average) for each month.
 
     Args:
-        df (pd.DataFrame): The prepared birth data. Requires columns: 
-            "conc_yy", "conc_month", "dob_yy", "birth_month", "conc_mm", "dob_mm", "births",
+        df (pd.DataFrame): The prepared birth data (created using df_birth_no_geo_prep). 
+            Requires columns: "conc_yy", "conc_month", "dob_yy", "birth_month", 
+            "conc_mm", "dob_mm", "births",
         years_greater_than (int): Only include data from above this year.
 
     """
-    df["avg_births"] = df.groupby(["dob_yy", "dob_mm"])["births"].transform("mean")
-    df["percent_above_average"] = (
-        df["births"] / df["avg_births"] - 1
-    ).round(2)
+    # group by month
+    df = df.groupby([
+        'conc_yy', 'conc_month','dob_yy',
+        'birth_month','conc_mm','dob_mm',], as_index=False).sum()
+
+    df = df.sort_values(by=['dob_yy','dob_mm']).reset_index(drop=True)
+
+    # add 12 month rolling average
+    df['avg_births'] = df['births'].rolling(window=12).mean() 
+    df['percent_above_avg'] = (df['births'] - df['avg_births'])/df['avg_births']*100
+
+    # only select dates > years_greater_than
+    df = df[df['dob_yy'] > years_greater_than]
 
     return df
 
@@ -208,6 +218,135 @@ def plot_births_by_month(df, year=1990, path_save_dir=None, dpi=300, save_plot=T
         plt.show()
 
 
+def plot_births_by_month_percent_above_avg(df, start_year=1981, end_year=2020, path_save_dir=None, dpi=300, save_plot=True):
+    """"Take the prepared df and output the violin plot of percentage change by month"""
+
+    dfp = percentage_birts_by_month(df, years_greater_than=1980)
+    dfp = dfp[(dfp['dob_yy'] >= start_year) & (dfp['dob_yy'] <= end_year)]
+
+    # round to 2 decimal places to look nice
+    dfp['percent_above_avg'] = np.round(dfp['percent_above_avg'],2)
+
+    print(dfp['dob_yy'].max())
+
+    TITLE = f"Monthly Increase/Decrease in Births, {start_year}-{end_year}"
+    X_LABEL = 'Percentage Above/Below Yearly Average'
+
+    # make plotly violin plot
+    fig = go.Figure(data=go.Violin(x=dfp["percent_above_avg"],
+                                y=dfp['birth_month'],
+                                
+                                # add a custom text using list comprehension and zip
+                                text = [f'{month}, {v:.2f}%' for month, v, in 
+                                        zip(dfp['dob_yy'],dfp['percent_above_avg'])],
+                                
+                                hoverinfo='text', # show the custom text for hover
+                                orientation='h', #horizontal orientation
+                                box_visible=False, # don't show box-plot
+                                meanline_visible=False, # hide meanline in violins
+                                line_color='dimgrey', 
+                                fillcolor='white',
+                                opacity=1,
+                                marker_symbol="circle",
+                                marker_color='dimgrey',
+                                marker_opacity=0.3, 
+                                marker_size=10,
+                                pointpos=0, # put scatters in middle of violin
+                                jitter=0.7, 
+                                scalemode='width',
+                                width=1.2,
+                                points='all',
+                                ))
+
+
+    # add shaded regions
+    shaded_region_list = []
+
+    # get min/max for shaded region
+    xmin = np.min(dfp['percent_above_avg'])*1.3
+    xmax = np.max(dfp['percent_above_avg'])*1.3
+
+    # negative birth percentage area
+    shaded_region_list.append(
+                    dict(
+                        type="rect",
+                        xref="x",
+                        yref="paper",
+                        x0=xmin*0.95,
+                        y0=0,
+                        x1=0,
+                        y1=1,
+                        fillcolor="red",
+                        opacity=0.1,
+                        layer="below",
+                        line_width=0,
+                    )
+                )
+
+    # positive birth percentage area
+    shaded_region_list.append(
+                    dict(
+                        type="rect",
+                        xref="x",
+                        yref="paper",
+                        x0=0,
+                        y0=0,
+                        x1=xmax*0.95,
+                        y1=1,
+                        fillcolor="green",
+                        opacity=0.1,
+                        layer="below",
+                        line_width=0,
+                    )
+                )
+
+    # set "tight" layout https://community.plotly.com/t/plt-tight-layout-in-plotly/10204/2
+    fig.update_layout(yaxis_zeroline=False, 
+                    width=600, 
+                    height=700,
+                    template='plotly_white',
+                    margin=dict(l=2, r=2, t=25, b=2), # create a "tight" layout
+                    xaxis=dict(range=[xmin, xmax], tickvals = [-10, -5, 0, 5, 10]),
+                    title=TITLE,
+                    title_x=0.55, # title position
+                    titlefont=dict(family='DejaVu Sans', color='#333333', size=16), 
+                    shapes=shaded_region_list
+                    )
+
+
+    # update x and y axes
+    # https://plotly.com/python/axes/
+    fig.update_xaxes(showgrid=False, 
+                    zeroline=True,
+                    zerolinecolor='lightgrey',
+                    zerolinewidth=2,
+                    title_text=X_LABEL,
+                    tickfont=dict(family='DejaVu Sans', color='#333333', size=14), # set custom font
+                    titlefont=dict(family='DejaVu Sans', color='#333333', size=14),
+                    fixedrange=False # allow zooming by not fixing range
+                    )
+
+    fig.update_yaxes(tickfont=dict(family='DejaVu Sans', color='#333333', size=14), 
+                    fixedrange=False
+                    )
+
+    # other config options: https://plotly.com/python/configuration-options/
+    # fig.show(config={"displayModeBar": False, "showTips": False})
+
+    # save fig as a png
+    if save_plot:
+        if path_save_dir is None:
+            path_save_dir = Path.cwd().parent.parent
+
+        fig.write_image(path_save_dir / f"{start_year}-{end_year}_births_by_month_percent_above_avg.png", scale=2)
+    else:
+        pass
+
+
+
+
+
+
 def main():
     logger = logging.getLogger(__name__)
     logger.info("making figures from consolidated tables")
@@ -220,11 +359,13 @@ def main():
         by=["dob_yy", "dob_mm"]
     )
     # create dfp
-    dfp = df_birth_no_geo_prep(df)
+    df = df_birth_no_geo_prep(df)
 
     plot_births_by_month(
-        dfp, year=1990, path_save_dir=path_save_dir, dpi=300, save_plot=True
+        df, year=1990, path_save_dir=path_save_dir, dpi=300, save_plot=True
     )
+
+    plot_births_by_month_percent_above_avg(df, start_year=1981, end_year=2020, path_save_dir=path_save_dir, dpi=300, save_plot=True)
 
 
 if __name__ == "__main__":
