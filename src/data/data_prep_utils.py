ad6aa56d8bc3c8d85bf2efdc478dcc0e031b2665
tvhahn <tvhahn@gmail.com>
2022-01-22 12:40:17 -0800

    moved functions to data_prep_utils.py

--- src/data/data_prep_utils.py
+++ src/data/data_prep_utils.py
@@ -6,6 +6,8 @@ import pathlib
 import os
 import zipfile
 from multiprocessing import Pool
+import datetime
+from dateutil.relativedelta import relativedelta
 
 
 def df_from_csv_with_geo(file_path, nrows=None):
@@ -385,3 +387,122 @@ def df_from_csv_no_geo_extra(file_path, nrows=None):
     return df[["dob_yy", "dob_mm", "apgar5", "births"]].astype(
         {"dob_mm": int, "dob_yy": int, "apgar5": int, "births": int}
     )
+
+###############################################################################
+# Functions for processing the collated data files and returning a dataframe
+###############################################################################
+
+def get_month_string(cols):
+    month_index = int(cols[0])
+
+    # from https://stackoverflow.com/a/28446432/9214620
+    return datetime.date(1900, month_index, 1).strftime("%B")
+
+
+def get_conception_month(cols):
+    month_index = int(cols[0])
+    concept_date = datetime.date(1900, month_index, 1) + relativedelta(months=-9)
+
+    return concept_date.strftime("%B")
+
+
+def get_conception_year(cols):
+    month_index = int(cols[0])
+    year = int(cols[1])
+    concept_date = datetime.date(year, month_index, 1) + relativedelta(months=-9)
+
+    return int(concept_date.strftime("%Y"))
+
+
+def get_conception_month_index(cols):
+    month_index = int(cols[0])
+    concept_date = datetime.date(1900, month_index, 1) + relativedelta(months=-9)
+
+    return int(concept_date.strftime("%m"))
+
+
+def df_birth_no_geo_prep(df):
+    """Prepare the birth data for analysis.
+
+    Args:
+        df (pd.DataFrame): The raw birth data. Requires columns: dob_yy, dob_mm, births.
+        filter_year (int): The year to filter the data to.
+
+    """
+
+    # load csv is like this...
+    # df = pd.read_csv(data_file, dtype=int).sort_values(by=['dob_yy', 'dob_mm', 'dob_wk'])
+
+    # add conception month columns and birth month
+    df["conc_month"] = df[["dob_mm"]].apply(get_conception_month, axis=1)
+    df["birth_month"] = df[["dob_mm"]].apply(get_month_string, axis=1)
+    df["conc_mm"] = df[["dob_mm"]].apply(get_conception_month_index, axis=1)
+    df["conc_yy"] = df[["dob_mm", "dob_yy"]].apply(get_conception_year, axis=1)
+    df = df.sort_values(by=["conc_yy", "conc_mm"])
+
+    return df[
+        [
+            "conc_yy",
+            "conc_month",
+            "dob_yy",
+            "birth_month",
+            "conc_mm",
+            "dob_mm",
+            "births",
+        ]
+    ]
+
+
+def filter_by_year(df, filter_cat="conc_yy", year=1990):
+    """Filter df by year, either with conception year ('conc_yy')
+    or birth year ('dob_yy')
+    """
+    df = (
+        df[df[filter_cat] == year]
+        .groupby(
+            [
+                "conc_yy",
+                "conc_month",
+                "dob_yy",
+                "birth_month",
+                "conc_mm",
+                "dob_mm",
+            ],
+            as_index=False,
+        )
+        .sum()
+    )
+    if filter_cat == "conc_yy":
+        df = df.sort_values(by=["conc_mm"]).reset_index(drop=True)
+        return df
+    else:
+        df = df.sort_values(by=["dob_mm"]).reset_index(drop=True)
+        return df
+
+
+def percentage_birts_by_month(df, years_greater_than=1980):
+    """Take the prepared df and calculage the average births (avg_births)
+    and percent above average (percent_above_average) for each month.
+
+    Args:
+        df (pd.DataFrame): The prepared birth data (created using df_birth_no_geo_prep). 
+            Requires columns: "conc_yy", "conc_month", "dob_yy", "birth_month", 
+            "conc_mm", "dob_mm", "births",
+        years_greater_than (int): Only include data from above this year.
+
+    """
+    # group by month
+    df = df.groupby([
+        'conc_yy', 'conc_month','dob_yy',
+        'birth_month','conc_mm','dob_mm',], as_index=False).sum()
+
+    df = df.sort_values(by=['dob_yy','dob_mm']).reset_index(drop=True)
+
+    # add 12 month rolling average
+    df['avg_births'] = df['births'].rolling(window=12).mean() 
+    df['percent_above_avg'] = (df['births'] - df['avg_births'])/df['avg_births']*100
+
+    # only select dates > years_greater_than
+    df = df[df['dob_yy'] > years_greater_than]
+
+    return df
